2f4a05c4bbd75fc3b605835db66e015e081ab1b8
Ericsson715 <92638586+Ericsson715@users.noreply.github.com>
2022-05-14 03:13:34 -0500

    Creacion de rama ABEL_CITAS y diseño inicial de las citas para el padre. app funcionando

---
+++ app/src/main/java/com/example/aprende_play/Generador_citas/Nueva_cita.java
@@ -0,0 +1,16 @@
+package com.example.aprende_play.Generador_citas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.aprende_play.R;
+
+public class Nueva_cita extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nueva_cita);
+    }
+}
